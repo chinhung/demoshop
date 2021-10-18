5dfca8ab446e74be0f5e0d39d5668e9d4378dbc4
chinhung <chinhung101@gmail.com>
2021-10-18 16:17:43 +0800

    Remove unused import statement

--- src/test/java/net/chinhung/application/product/lazy_stock_quantity/StockQuantityStoreThreadTest.java
+++ src/test/java/net/chinhung/application/product/lazy_stock_quantity/StockQuantityStoreThreadTest.java
@@ -11,7 +11,6 @@ import java.util.List;
 import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 import static util.TestUtil.ANY_STRING;
 
 public class StockQuantityStoreThreadTest {
